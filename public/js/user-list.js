d80e02febcdae2d8660aad18caa142482d3da3c6
queuecat <2356583659@qq.com>
2021-05-25 10:41:36 +0800

    fix:题解列表页标切换当数据为空时消失

--- public/js/user-list.js
+++ public/js/user-list.js
@@ -3,106 +3,112 @@ var id = window.user._id;
 var page = 1;
 // 渲染列表
 $.ajax({
-    type: 'get',
-    url: '/public/answers',
-    data: {
-        userId: id,
-        page: 1,
-        count: 10,
-        sort: 0
-    },
-    success: (res) => {
-        // $('#thumbnail').val(res[0].cover)
-        // alert('修改成功');
-        console.log(res);
-        page = res.page;
-        var list = template('listTpl', res);
-        $('#listBox').html(list);
-        var page = template('pageTpl', res);
-        $('#pageBox').html(page);
-    },
-    error: (xhr) => {
-        alert(JSON.parse(xhr.responseText).message);
+	type: 'get',
+	url: '/public/answers',
+	data: {
+		userId: id,
+		page: 1,
+		count: 10,
+		sort: 0,
+	},
+	success: (res) => {
+		// $('#thumbnail').val(res[0].cover)
+		// alert('修改成功');
+		console.log(res);
+		if (res.pages.length === 0) {
+			res.pages.push('1');
+			res.tailPage = 1;
+		}
+		page = res.page;
+		var list = template('listTpl', res);
+		$('#listBox').html(list);
+		var page = template('pageTpl', res);
+		$('#pageBox').html(page);
+	},
+	error: (xhr) => {
+		alert(JSON.parse(xhr.responseText).message);
 
-        // alert(JSON.parse(xhr.responseText).message);
-
-    }
-
+		// alert(JSON.parse(xhr.responseText).message);
+	},
 });
 
 function ajaxreload() {
-    $.ajax({
-        type: 'get',
-        url: '/public/answers',
-        data: {
-            userId: id,
-            page: 1,
-            count: 10,
-            sort: 0
-        },
-        success: (res) => {
-            // $('#thumbnail').val(res[0].cover)
-            // alert('修改成功');
-            console.log(res);
-            page = res.page;
-            var list = template('listTpl', res);
-            $('#listBox').html(list);
-            var page = template('pageTpl', res);
-            $('#pageBox').html(page);
-        },
-        error: (xhr) => {
-            alert(JSON.parse(xhr.responseText).message);
+	$.ajax({
+		type: 'get',
+		url: '/public/answers',
+		data: {
+			userId: id,
+			page: 1,
+			count: 10,
+			sort: 0,
+		},
+		success: (res) => {
+			// $('#thumbnail').val(res[0].cover)
+			// alert('修改成功');
+			console.log(res);
+			if (res.pages.length === 0) {
+				res.pages.push('1');
+				res.tailPage = 1;
+			}
+			page = res.page;
+			var list = template('listTpl', res);
+			$('#listBox').html(list);
+			var page = template('pageTpl', res);
+			$('#pageBox').html(page);
+		},
+		error: (xhr) => {
+			alert(JSON.parse(xhr.responseText).message);
 
-            // alert(JSON.parse(xhr.responseText).message);
-
-        }
-
-    });
+			// alert(JSON.parse(xhr.responseText).message);
+		},
+	});
 }
 
 function changePage(page) {
-    console.log(page);
-    $.ajax({
-        type: 'get',
-        url: '/public/answers',
-        data: {
-            userId: id,
-            page,
-            count: 10,
-            sort: 0
-        },
-        success: (res) => {
-            page = res.page;
-            console.log(res);
-            var list = template('listTpl', res);
-            $('#listBox').html(list);
-            var page = template('pageTpl', res);
-            $('#pageBox').html(page);
-
-        },
-        error: (xhr) => {
-            alert(JSON.parse(xhr.responseText).message);
-        }
-    });
+	console.log(page);
+	$.ajax({
+		type: 'get',
+		url: '/public/answers',
+		data: {
+			userId: id,
+			page,
+			count: 10,
+			sort: 0,
+		},
+		success: (res) => {
+			page = res.page;
+			console.log(res);
+			if (res.pages.length === 0) {
+				res.pages.push('1');
+				res.tailPage = 1;
+			}
+			var list = template('listTpl', res);
+			$('#listBox').html(list);
+			var page = template('pageTpl', res);
+			$('#pageBox').html(page);
+		},
+		error: (xhr) => {
+			alert(JSON.parse(xhr.responseText).message);
+		},
+	});
 }
 // 删除题解
 function deleteF(id) {
-    if (confirm('是否删除该文章？')) {
-        $.ajax({
-            type: 'DELETE',
-            url: '/userAction/answers/' + id,
-            success: (res) => {
-                // alert('删除成功');
-                changePage(page);
-
-            },
-            error: (xhr) => {
-                alert(JSON.parse(xhr.responseText).message);
-            }
-        });
-    }
+	if (confirm('是否删除该文章？')) {
+		$.ajax({
+			type: 'DELETE',
+			url: '/userAction/answers/' + id,
+			success: (res) => {
+				// alert('删除成功');
+				changePage(page);
+			},
+			error: (xhr) => {
+				alert(JSON.parse(xhr.responseText).message);
+			},
+		});
+	}
 }
 
 function jump(url) {
-    location.href = url;
+	location.href = url;
 }
